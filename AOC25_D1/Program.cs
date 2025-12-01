165a1d24eab0c7303dddc0d24e137f5617c011de
mackb <mackbakkum@gmail.com>
2025-12-01 17:00:45 +0100

    D1_V1

--- AOC25_D1/Program.cs
+++ AOC25_D1/Program.cs
@@ -1,3 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.IO;
+using System.Collections.Generic;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    private static void Main()
+    {
+        var input = File.ReadAllLines("../../../d1_input.txt");
+
+        const int start = 50;
+        const int neutral = 0;
+        const int maxKnob = 100;
+
+        var zeroCounter = CalculateZeros(input, start, neutral, maxKnob);
+        
+        Console.WriteLine(zeroCounter);
+    }
+
+    
+    private static int CalculateZeros(string[] commands, int start, int neutral, int maxKnob)
+    {
+        var directionList = new List<char>();
+        var stepsList = new List<int>();
+
+        foreach (var line in commands)
+        {
+            directionList.Add(line[0]);
+            stepsList.Add(int.Parse(line[1..]));
+        }
+
+        var knobPosition = start;
+        var zeroCounter = 0;
+
+        for (int i = 0; i < directionList.Count; i++)
+        {
+            if (directionList[i] == 'L')
+            {
+                knobPosition = (knobPosition - stepsList[i]) % maxKnob;
+                if (knobPosition < 0)
+                {
+                    knobPosition += maxKnob;
+                }
+            }
+            else if (directionList[i] == 'R')
+            {
+                knobPosition = (knobPosition + stepsList[i]) % maxKnob;
+            }
+
+            if (knobPosition == neutral)
+            {
+                zeroCounter++;
+            }
+        }
+
+        return zeroCounter;
+    }
+}
